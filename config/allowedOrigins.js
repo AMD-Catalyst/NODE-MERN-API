9977554422387c86f870e14f5f7c3cbde916afba
alteredsky28 <ronneldaveticlao@gmail.com>
2022-12-23 23:10:26 +0800

    Config Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["https://mern-g6np.onrender.com"];
+const allowedOrigins = [
+  "35.160.120.126",
+  "44.233.151.27",
+  "34.211.200.85",
+  "https://mern-g6np.onrender.com",
+];
 
 module.exports = allowedOrigins;
